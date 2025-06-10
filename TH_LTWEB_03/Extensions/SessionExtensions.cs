47d077e8b3431a5a608b73ed061b84d1754dd0bd
TranTrungVietHoang <trantrungviethoang@gmail.com>
2025-06-10 11:08:56 +0700

    gio hang

---
+++ TH_LTWEB_03/Extensions/SessionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TH_LTWEB_03.Models;
+using TH_LTWEB_03.Repositories;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Authorization;
+using TranTrungVietHoang.Models;
+using System.Text.Json;
+
+
+
+namespace TranTrungVietHoang.Extensions
+{
+    public static class SessionExtensions
+    {
+        public static void SetObjectAsJson(this ISession session, string key, object value)
+        {
+            session.SetString(key, JsonSerializer.Serialize(value));
+        }
+        public static T GetObjectFromJson<T>(this ISession session,
+        string key)
+        {
+            var value = session.GetString(key);
+            return value == null ? default :
+            JsonSerializer.Deserialize<T>(value);
+        }
+    }
+}
